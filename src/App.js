e973d0879f6a1daa3bc34c033cc09d1702266b57
phanlongtelio <hailong1@telio.vn>
2022-03-11 13:08:50 +0700

    Rename files

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import { Route,Switch } from 'react-router-dom';
-import './App.css';
-import HeaderContainer from './containers/header/HeaderContainer';
-import HomeContainer from './containers/home/HomeContainer';
-import LoginPageContainer from './containers/loginPage/LoginPageContainer';
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+import HeaderContainer from "./containers/header/HeaderContainer";
+import HomeContainer from "./containers/HomePage/HomeContainer";
+import LoginPageContainer from "./containers/LoginPage/LoginPageContainer";
 function App() {
   return (
     <div className="App">
       <Switch>
-        <Route path='/login'>
-          <LoginPageContainer/>
+        <Route path="/login">
+          <LoginPageContainer />
         </Route>
         <Route path="/home">
-            <HomeContainer/>
+          <HomeContainer />
         </Route>
       </Switch>
     </div>
