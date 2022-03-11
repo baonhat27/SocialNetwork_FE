01066acbcc655f93dc70eda00c2fbe57e5200b4c
Bao Nhat <vophungbaonhat@gmail.com>
2022-03-11 14:33:57 +0700

    Create dev branch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { Route,Switch } from 'react-router-dom';
 import './App.css';
-import HeaderContainer from './containers/header/HeaderContainer';
 import HomeContainer from './containers/home/HomeContainer';
 import LoginPageContainer from './containers/loginPage/LoginPageContainer';
 function App() {
