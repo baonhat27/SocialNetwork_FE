592eca616e42236182997e16692436769c05e89c
phanlongtelio <hailong1@telio.vn>
2022-03-15 08:55:00 +0700

    Rename file

--- src/containers/loginPage/component/loginPage.js
+++ src/containers/loginPage/component/loginPage.js
@@ -1,116 +1,39 @@
-import styles from "./LoginPage.module.css";
-import React from "react";
-import Input from "../../../components/Input/Input";
-import Button from "../../../components/Button/Button";
+import styles from './LoginPageComponent.module.css';
+import React from 'react';
+import Input from '../../../components/Input/Input';
+import Button from '../../../components/Button/Button';
 
 const LoginPage = (props) => {
-  return (
-    <div className={styles.backgroundLogin}>
-      <div className={styles.loginPage}>
-        <div className={styles.social}></div>
-        <div className={styles.box}>
-          <div
-            className={
-              props.showSignUp
-                ? styles.scroll + " " + styles.show
-                : styles.scroll
-            }
-          >
-            <div className={styles.login}>
-              <div style={{ height: "450px" }}>
-                <div className={styles.headerForm}>
-                  <span className={styles.heading}>ĐĂNG NHẬP</span>
-                  <p className={styles.link}>
-                    Bạn chưa có tài khoản
-                    <br />
-                    <span
-                      className={styles.move}
-                      onClick={function () {
-                        props.setShowSignUp(true);
-                      }}
-                    >
-                      {" "}
-                      Đăng kí ngay
-                    </span>
-                  </p>
-                </div>
-                <span className={styles.loginInputHeading}>Tên đăng nhập</span>
-                <span className={styles.loginInputPassword}>Mật khẩu</span>
-                <Input
-                  placeholder="Tên đăng nhập"
-                  value={props.username}
-                  onChange={props.handleUsername}
-                ></Input>
-                <Input
-                  value={props.password}
-                  onChange={props.handlePassword}
-                  type="password"
-                  placeholder="Mật khẩu"
-                ></Input>
-                <p className={styles.p}>Quên mật khẩu?</p>
-                <Button
-                  style={{ background: "linear-gradient(#9bc1f2,#1877f2)" }}
-                  onClick={props.login}
-                >
-                  Đăng nhập
-                </Button>
-              </div>
+    return (
+        <div className={styles.loginPage}>
+            <div className={styles.social}>
+                <h1 className={styles.heading}>Social-network</h1>
+                <p className={styles.describes}>Social-network giúp bạn kết nối và chia sẻ với mọi người trong cuộc sống của bạn.</p>
             </div>
-            <div className={styles.signup}>
-              <div style={{ height: "550px", marginTop: "80px" }}>
-                <div className={styles.headerForm}>
-                  <span className={styles.heading}>ĐĂNG KÝ</span>
-                  <p
-                    className={styles.link}
-                    style={{ position: "relative", left: "180px" }}
-                  >
-                    Bạn đã có tài khoản
-                    <br />
-                    <span
-                      className={styles.move}
-                      onClick={function () {
-                        props.setShowSignUp(false);
-                      }}
-                    >
-                      {" "}
-                      Đăng nhập ngay
-                    </span>
-                  </p>
+            <div className={styles.box}>
+                <div className={props.showSignUp?styles.scroll+" "+styles.show:styles.scroll}>
+                    <div className={styles.login}>
+                        <h2>Đăng nhập</h2>
+                        <Input placeholder="Tên đăng nhập" value={props.username} onChange={props.handleUsername}/>
+                        <Input value={props.password} onChange={props.handlePassword} type="password" placeholder="Mật khẩu"/>
+                        <Button style={{background:"#1877f2"}}>Đăng nhập</Button>
+                        <p className={styles.p}>Quên mật khẩu?</p>
+                        <div className={styles.line}></div>
+                        <Button style={{background:"#42b72a"}} onClick={function(){props.setShowSignUp(true)}}>Tạo tài khoản mới</Button>
+                    </div>
+                    <div className={styles.signup}>
+                        <h2>Đăng ký tài khoản mới</h2>
+                        <Input value={props.username} onChange={props.handleUsername} placeholder="Tên đăng nhập"/>
+                        <Input value={props.password} onChange={props.handlePassword} type="password" placeholder="Mật khẩu"/>
+                        <Input value={props.retypePassword} onChange={props.handleRetypePassword} type="password" placeholder="Nhập lại mật khẩu"/>
+                        <Button style={{background:"#1877f2"}}>Đăng ký</Button>
+                        <div className={styles.line} style={{marginTop:"30px"}}></div>
+                        <Button style={{background:"#42b72a"}} onClick={function(){props.setShowSignUp(false)}}>Đăng nhập</Button>
+                    </div>
                 </div>
-                <span className={styles.loginInputHeading}>Tên đăng nhập</span>
-                <span className={styles.loginInputPassword}>Mật khẩu</span>
-
-                <Input
-                  value={props.username}
-                  onChange={props.handleUsername}
-                  placeholder="Tên đăng nhập"
-                />
-                <Input
-                  value={props.password}
-                  onChange={props.handlePassword}
-                  type="password"
-                  placeholder="Mật khẩu"
-                />
-                <Input
-                  value={props.retypePassword}
-                  onChange={props.handleRetypePassword}
-                  type="password"
-                  placeholder="Nhập lại mật khẩu"
-                />
-
-                <Button
-                  style={{ background: "linear-gradient(#9bc1f2,#1877f2)" }}
-                >
-                  Đăng ký
-                </Button>
-              </div>
             </div>
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default LoginPage;
-
