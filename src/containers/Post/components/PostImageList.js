948d56ccc63c371c5fca700609502b348703ac03
Phan Đình Hải Long From Telio <hailong1@telio.vn>
2022-03-17 02:32:06 +0000

    Update ui

--- src/containers/Post/components/PostImageList.js
+++ src/containers/Post/components/PostImageList.js
@@ -1,18 +1,35 @@
 import styles from "./styles.module.css";
-
+import { useState } from "react";
 import { getImage } from "../../../shared/service";
 
 export default function PostImageList({ images }) {
+  const firstImage = images[0];
+  const restImages = images.slice(1);
+  const [showMore, setShowMore] = useState(() => restImages.length !== 0);
+  const [displayMore, setDisplayMore] = useState(false);
+
+  const onClickShowMore = () => {
+    setShowMore(false);
+    setDisplayMore(true);
+  };
+
   return (
     <div className={styles.list_image}>
-      {images.map((image, index) => (
-        <img
-          className={styles.image}
-          key={index}
-          src={getImage(image)}
-          alt=""
-        />
-      ))}
+      <img className={styles.image} src={getImage(firstImage)} alt="" />
+      {showMore && (
+        <button className={styles.btn} onClick={onClickShowMore}>
+          {`Show ${restImages.length} more`}
+        </button>
+      )}
+      {displayMore &&
+        restImages.map((image, index) => (
+          <img
+            className={styles.image}
+            key={index}
+            src={getImage(image)}
+            alt=""
+          />
+        ))}
     </div>
   );
 }
