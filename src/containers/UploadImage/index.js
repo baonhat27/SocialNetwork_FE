c3cd9a1f88505eebb6c79980551fd80b3aab5729
Phan Đình Hải Long From Telio <hailong1@telio.vn>
2022-03-17 09:39:46 +0000

    Add upload container

---
+++ src/containers/UploadImage/index.js
@@ -0,0 +1,45 @@
+import { useEffect, useRef, useState } from "react";
+import { getImage, uploadImages } from "../../shared/service";
+
+const withUploadImage = (Component) => {
+  const UploadImage = (props) => {
+    const [images, setImages] = useState([]);
+    const [isUploading, setIsUploading] = useState(false);
+
+    const inputRef = useRef(null);
+
+    const handleUpload = () => {
+      inputRef.current.click();
+      inputRef.current.onchange = function (e) {
+        const files = Array.from(inputRef.current.files);
+        uploadImages(files).then((res) => {
+          setImages(res.data.map((img) => getImage(img)));
+          setIsUploading(false);
+        });
+      };
+      setIsUploading(true);
+    };
+
+    return (
+      <>
+        <input
+          type="file"
+          ref={inputRef}
+          id="uploadImages"
+          name="images"
+          accept="images/*"
+          multiple
+        />
+        <Component
+          images={images}
+          uploading={isUploading}
+          upload={handleUpload}
+          {...props}
+        />
+      </>
+    );
+  };
+  return UploadImage;
+};
+
+export default withUploadImage;
