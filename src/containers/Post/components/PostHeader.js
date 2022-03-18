733f096d9e5032b5a9ca7931a25cf12d50920cfb
Phan Đình Hải Long From Telio <hailong1@telio.vn>
2022-03-18 07:07:09 +0000

    Update ui

--- src/containers/Post/components/PostHeader.js
+++ src/containers/Post/components/PostHeader.js
@@ -1,31 +1,24 @@
-import { useState } from "react";
-
 import styles from "./styles.module.css";
 import { timeFromNow } from "../../../shared/utils";
 import clockLogo from "../../../shared/image/clock.png";
 import defaultUserAvatar from "../../../shared/image/user.png";
-import { useEffect } from "react";
-import { getUserProfile } from "../../../shared/service";
-
-export default function PostHeader({ userId, createdAt }) {
-  const [user, setUser] = useState();
-
-  useEffect(() => {
-    getUserProfile(userId).then((res) => setUser(res.data));
-  }, []);
-
-  return user ? (
+export default function PostHeader({ user, createdAt }) {
+  return (
     <div className={styles.header}>
-      <img className={styles["avatar"]} src="https://joeschmoe.io/api/v1/random" alt="" />
+      <img
+        className={styles["avatar"]}
+        src={user.avatar === "no infromation" ? defaultUserAvatar : user.avatar}
+        alt=""
+      />
       <div className={styles.info}>
-        <span className={styles.user_name}>Nhat Bao</span>
+        <span
+          className={styles.user_name}
+        >{`${user.firstName} ${user.lastName}`}</span>
         <div className={styles.time_box}>
           <img className={styles["clock"]} src={clockLogo} alt="" />
           <span className={styles.time}>{timeFromNow(createdAt)}</span>
         </div>
       </div>
     </div>
-  ) : (
-    "Loading"
   );
 }
--- src/containers/Post/index.js
+++ src/containers/Post/index.js
@@ -1,35 +1,18 @@
-import { useState, useEffect } from "react";
-import { getUserProfile } from "../../shared/service"
 import PostHeader from "./components/PostHeader";
 import styles from "./styles.module.css";
-import PostContent from "./components/PostContent"
-import PostImageList from "./components/PostImageList"
+import PostContent from "./components/PostContent";
+import PostImageList from "./components/PostImageList";
 import Comment from "./components/Comment";
 import ReactionBar from "./components/Reactions";
-import { useDispatch, useSelector } from "react-redux";
-import { createComment } from "../../shared/store/redux/actions";
+
 function Post({ post }) {
-  const [user, setUser] = useState();
-  const [error, setError] = useState("");
-  const dispatch = useDispatch()
-  useEffect(() => {
-    (async () => {
-      const userRes = await getUserProfile(post.createdBy);
-      if (userRes.status === 200) {
-        setUser(userRes.data);
-      } else {
-        setError(userRes.data);
-      }
-    })()
-    }, []);
-    
-    return (
-      <div className={styles.post}>
-      <PostHeader userId={post.createdBy} createdAt={post.createdAt} />
+  return (
+    <div className={styles.post}>
+      <PostHeader user={post.createdBy} createdAt={post.createdAt} />
       <PostContent content={post.content} />
       {post.images.length !== 0 && <PostImageList images={post.images} />}
-      <ReactionBar/>
-      <Comment postId={post._id}/>
+      <ReactionBar />
+      <Comment postId={post._id} />
     </div>
   );
 }
